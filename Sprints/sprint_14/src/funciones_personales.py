6f6703d5cec86142378dbabdc808f75fecb02547
Pablo Vazquez <alienfibio@gmail.com>
2026-07-17 22:00:53 -0600

    Se terminó el Proyecto de álgebra lineal Concluye Sprint 14, comienza el 15

--- Sprints/sprint_14/src/funciones_personales.py
+++ Sprints/sprint_14/src/funciones_personales.py
@@ -1,5 +1,6 @@
+import numpy as np
 from sklearn.neighbors import NearestNeighbors
-
+import sklearn.metrics
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import f1_score
 
@@ -54,3 +55,22 @@ def evaluate_knn(X_train, X_test, y_train, y_test):
         scores.append(f1_score(y_test, pred))
 
     return scores
+#====================================
+def eval_classifier(y_true, y_pred):
+    
+    f1_score = sklearn.metrics.f1_score(y_true, y_pred)
+    print(f'F1: {f1_score:.2f}')
+    
+# si tienes algún problema con la siguiente línea, reinicia el kernel y ejecuta el cuaderno de nuevo
+    cm = sklearn.metrics.confusion_matrix(y_true, y_pred, normalize='all')
+    print('Matriz de confusión')
+    print(cm)
+#====================================
+def rnd_model_predict(P, size, seed=42):
+    """ Genera predicciones aleatorias para un
+     clasificador binario con probabilidad P de
+     predecir 1. 
+     """
+
+    rng = np.random.default_rng(seed=seed)
+    return rng.binomial(n=1, p=P, size=size)
